809c34c6bfc232a5c498c1a305180a3a8f4141ee
BeckyPachtman <beckypdev@gmail.com>
2020-09-23 21:16:13 +0300

    moved all files from HTML file folder to newly created views folder

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
+
 app.get('/', function(req, res){
-    //res.sendFile('HTML files/index.html')
+    res.render('index.html')
 })
 
 app.listen(3000)
